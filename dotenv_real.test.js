a2b3fcc91ef986cf4641ced52687fc0cc3ffb22a
Jae-cheol Kim <forcemax@gmail.com>
2020-04-03 20:29:39 +0900

    add dotenv real test

---
+++ dotenv_real.test.js
@@ -0,0 +1,34 @@
+const process = require('process');
+const path = require('path');
+const cp = require('child_process');
+const dotenv = require('dotenv').config();
+const index = require('./index.js');
+
+jest.setTimeout(600000);
+
+const ts_id = "799632";
+
+test('input parameter check', async() => {
+    const ip = path.join(__dirname, 'index.js');
+    let output;
+    try {
+        output = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+    } catch (ex) {
+        output = ex.stdout.toString();
+    }
+    console.log(output);
+});
+
+test('check complete', async() => {
+    let http_promise_check = index.check_complete(process.env.INPUT_ACCESS_KEY, ts_id);
+    let ret_check = await http_promise_check;
+
+    console.log(ret_check);
+});
+
+test('get test result', async() => {
+    let http_promise_check = index.get_test_result(process.env.INPUT_ACCESS_KEY, ts_id);
+    let ret_check = await http_promise_check;
+
+    console.log(ret_check);
+});
